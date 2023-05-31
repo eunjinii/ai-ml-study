3786791e8726eda24e52b78b149aa877825bb0f4
eunjinii <lucy7xoxo@naver.com>
2023-05-31 13:56:02 +0000

    capsnet visualization code fix

---
+++ assignment/2023-05-31-capsnet/capsnet-mnist-test.py
@@ -0,0 +1,287 @@
+import numpy as np
+import random
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+import time
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+from torch.profiler import profile, record_function, ProfilerActivity
+import torch.autograd.profiler as profiler
+#import squash0_cuda
+import torch.cuda.nvtx as nvtx
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class PrimaryCaps(nn.Module):
+    """Primary capsule layer."""
+
+    def __init__(self, num_conv_units, in_channels, out_channels, kernel_size, stride):
+        
+        super(PrimaryCaps, self).__init__()
+
+        # Each conv unit stands for a single capsule.
+        self.conv = nn.Conv2d(in_channels=in_channels,
+                                  out_channels=out_channels * num_conv_units,
+                                  kernel_size=kernel_size,
+                                  stride=stride)
+        self.out_channels = out_channels
+
+    def forward(self, x):
+      
+        # Shape of x: (batch_size, in_channels, height, weight)
+        # Shape of out: out_capsules * (batch_size, out_channels, height, weight)
+        out = self.conv(x)
+        # Flatten out: (batch_size, out_capsules * height * weight, out_channels)
+        batch_size = out.shape[0]
+        
+        
+        return squash(out.contiguous().view(batch_size, -1, self.out_channels))
+
+
+class DigitCaps(nn.Module):
+    """Digit capsule layer."""
+
+    def __init__(self, in_dim, in_caps, out_caps, out_dim, num_routing):
+        
+        """
+        Initialize the layer.
+        Args:
+            in_dim: 		Dimensionality of each capsule vector.
+            in_caps: 		Number of input capsules if digits layer.
+            out_caps: 		Number of capsules in the capsule layer
+            out_dim: 		Dimensionality, of the output capsule vector.
+            num_routing:	Number of iterations during routing algorithm
+        """
+        
+        super(DigitCaps, self).__init__()
+        self.in_dim = in_dim
+        self.in_caps = in_caps
+        self.out_caps = out_caps
+        self.out_dim = out_dim
+        self.num_routing = num_routing
+        self.device = device
+        self.W = nn.Parameter(0.01 * torch.randn(1, out_caps, in_caps, out_dim, in_dim),
+                                  requires_grad=True)
+
+    def forward(self, x):
+        
+        batch_size = x.size(0)
+        # (batch_size, in_caps, in_dim) -> (batch_size, 1, in_caps, in_dim, 1)
+        x = x.unsqueeze(1).unsqueeze(4)
+        # W @ x =
+        # (1, out_caps, in_caps, out_dim, in_dim) @ (batch_size, 1, in_caps, in_dim, 1) =
+        # (batch_size, out_caps, in_caps, out_dims, 1)
+        u_hat = torch.matmul(self.W, x)
+        # (batch_size, out_caps, in_caps, out_dim)
+        u_hat = u_hat.squeeze(-1)
+        # detach u_hat during routing iterations to prevent gradients from flowing
+        temp_u_hat = u_hat.detach()
+
+        b = torch.zeros(batch_size, self.out_caps, self.in_caps, 1).to(self.device)
+            
+     
+        for route_iter in range(self.num_routing - 1):
+            # (batch_size, out_caps, in_caps, 1) -> Softmax along out_caps
+            
+            c = b.softmax(dim=1)
+
+            # element-wise multiplication
+            # (batch_size, out_caps, in_caps, 1) * (batch_size, in_caps, out_caps, out_dim) ->
+            # (batch_size, out_caps, in_caps, out_dim) sum across in_caps ->
+            # (batch_size, out_caps, out_dim)
+            
+            s = (c * temp_u_hat).sum(dim=2)
+            # apply "squashing" non-linearity along out_dim
+            #with profiler.record_function("squash2"):
+            
+            v = squash(s)
+            # dot product agreement between the current output vj and the prediction uj|i
+            # (batch_size, out_caps, in_caps, out_dim) @ (batch_size, out_caps, out_dim, 1)
+            # -> (batch_size, out_caps, in_caps, 1)
+            
+            uv = torch.matmul(temp_u_hat, v.unsqueeze(-1))
+            
+            b += uv
+
+        # last iteration is done on the original u_hat, without the routing weights update
+         
+            c = b.softmax(dim=1)
+            
+            s = (c * u_hat).sum(dim=2)
+        # apply "squashing" non-linearity along out_dim
+        #with profiler.record_function("squash3"):
+            v = squash(s)
+        
+        return v
+
+
+class CapsNet(nn.Module):
+    """Basic implementation of capsule network layer."""
+
+    def __init__(self):
+        super(CapsNet, self).__init__()
+
+        # Conv2d layer
+        self.conv = nn.Conv2d(1, 256, 9)
+        self.relu = nn.ReLU(inplace=True)
+
+        # Primary capsule
+        self.primary_caps = PrimaryCaps(num_conv_units=32,
+                                        in_channels=256,
+                                        out_channels=8,
+                                        kernel_size=9,
+                                        stride=2)
+
+        # Digit capsule
+        self.digit_caps = DigitCaps(in_dim=8,
+                                    in_caps=32 * 6 * 6,
+                                    out_caps=10,
+                                    out_dim=16,
+                                    num_routing=3)
+
+        # Reconstruction layer
+        self.decoder = nn.Sequential(
+            nn.Linear(16 * 10, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 784),
+            nn.Sigmoid())
+
+    def forward(self, x):
+        out = self.relu(self.conv(x))
+        out = self.primary_caps(out)
+        out = self.digit_caps(out)
+
+        # Shape of logits: (batch_size, out_capsules)
+        logits = torch.norm(out, dim=-1)
+        pred = torch.eye(10).to(device).index_select(dim=0, index=torch.argmax(logits, dim=1))
+
+        # Reconstruction
+        batch_size = out.shape[0]
+        reconstruction = self.decoder((out * pred.unsqueeze(2)).contiguous().view(batch_size, -1))
+
+        return logits, reconstruction
+
+
+class CapsuleLoss(nn.Module):
+    """Combine margin loss & reconstruction loss of capsule network."""
+
+    def __init__(self, upper_bound=0.9, lower_bound=0.1, lmda=0.5):
+        super(CapsuleLoss, self).__init__()
+        self.upper = upper_bound
+        self.lower = lower_bound
+        self.lmda = lmda
+        self.reconstruction_loss_scalar = 5e-4
+        self.mse = nn.MSELoss(reduction='sum')
+
+    def forward(self, images, labels, logits, reconstructions):
+        # Shape of left / right / labels: (batch_size, num_classes)
+        left = (self.upper - logits).relu() ** 2  # True negative
+        right = (logits - self.lower).relu() ** 2  # False positive
+        margin_loss = torch.sum(labels * left) + self.lmda * torch.sum((1 - labels) * right)
+
+        # Reconstruction loss
+        reconstruction_loss = self.mse(reconstructions.contiguous().view(images.shape), images)
+
+        # Combine two losses
+        return margin_loss + self.reconstruction_loss_scalar * reconstruction_loss
+
+
+def squash(x, dim=-1):
+    """ return the squash function of x. """
+    #squared_norm = (x ** 2).sum(dim=dim, keepdim=True)
+    #scale = squared_norm / (1 + squared_norm)
+    #torch.nn.functional.
+    
+    a = torch.abs(x)
+    sq =  (x-1)-F.gelu(x)+torch.cos(x)-torch.relu(x)
+    
+    return sq
+
+
+def main():
+
+# Load model
+    
+    """ return the model accuracy of population. """
+    model = CapsNet().to(device)
+    criterion = CapsuleLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.96)
+
+    # Load data
+    transform = transforms.Compose([
+        # shift by 2 pixels in either direction with zero padding.
+        transforms.RandomCrop(28, padding=2),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    DATA_PATH = './data'
+    BATCH_SIZE = 128
+    train_loader = DataLoader(
+        dataset=MNIST(root=DATA_PATH, download=True, train=True, transform=transform),
+        batch_size=BATCH_SIZE,
+        num_workers=4,
+        shuffle=True)
+    test_loader = DataLoader(
+        dataset=MNIST(root=DATA_PATH, download=True, train=False, transform=transform),
+        batch_size=BATCH_SIZE,
+        num_workers=4,
+        shuffle=True)
+
+    # Train
+    EPOCHES = 20
+    model.train()
+    for ep in range(EPOCHES):
+        batch_id = 1
+        correct, total, total_loss = 0, 0, 0.
+        for images, labels in train_loader:
+            optimizer.zero_grad()
+            images = images.to(device)
+            labels = torch.eye(10).index_select(dim=0, index=labels).to(device)
+            logits, reconstruction = model(images)
+
+            # Compute loss & accuracy
+            loss = criterion(images, labels, logits, reconstruction)
+            correct += torch.sum(
+                torch.argmax(logits, dim=1) == torch.argmax(labels, dim=1)).item()
+            total += len(labels)
+            accuracy = correct / total
+            total_loss += loss
+            loss.backward()
+            optimizer.step()  
+            
+        print('Epoch {}, loss: {}, accuracy: {}'.format(ep + 1,
+                                                        total_loss / batch_id,
+                                                        correct / total))
+        scheduler.step(ep)
+
+    model.eval()
+    correct, total = 0, 0
+    for images, labels in test_loader:
+        # Add channels = 1
+        images = images.to(device)
+        # Categogrical encoding
+        labels = torch.eye(10).index_select(dim=0, index=labels).to(device) 
+        logits, reconstructions = model(images)
+        pred_labels = torch.argmax(logits, dim=1)
+        correct += torch.sum(pred_labels == torch.argmax(labels, dim=1)).item()
+        total += len(labels)
+    print('Accuracy: {}'.format(correct / total))
+    accuracy = correct / total
+    #print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=1000)) 
+    #print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=1000)) 
+   
+    return accuracy
+
+        
+
+if __name__ == '__main__':
+    main()
