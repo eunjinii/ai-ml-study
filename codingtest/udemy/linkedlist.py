fbcfe66faf090f4fbc06f7b3a1e8a54054c6687e
eunjinii <lucy7xoxo@naver.com>
2023-06-27 00:36:02 +0000

    linked list get set remove insert reverse

--- codingtest/udemy/linkedlist.py
+++ codingtest/udemy/linkedlist.py
@@ -70,6 +70,64 @@ class LinkedList:
 
         return temp
 
+    def get(self, index):
+        if index < 0 or index >= self.length:  # index 유효한지 확인해야 한다.
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set(self, index, value):
+        temp = self.get(index)  # get method reuse
+        if temp:  # get method reuse
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if self.length == 0:
+            return self.prepend(value)  # 재활용
+        if index == self.length:
+            return self.append(value)  # 재활용
+
+        new_node = Node(value)
+        pre = self.get(index - 1)
+        new_node.next = pre.next
+        pre.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None  # remove에서는 성공하면 Node를 return할 것이므로, 엣지케이스에서는 None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        target = pre.next  # better complexity than self.get(index)
+        pre.next = target.next
+        target.next = None
+        self.length -= 1
+        return target
+
+    def reverse(self):  # 매우 어려움 순서 주의 before, temp, after
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        after = temp.next
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp  # 갭 메꾸기
+            temp = after  # 갭 메꾸기
+
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -83,5 +141,8 @@ my_linked_list.append(9)
 my_linked_list.prepend(58)
 my_linked_list.pop()
 my_linked_list.pop_first()
+my_linked_list.set(1, 13)
+my_linked_list.insert(1, 90)
+my_linked_list.reverse()
 
 my_linked_list.print_list()
