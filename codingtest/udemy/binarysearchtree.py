395dbe3445fa854682905b3b6ac441d1242f64d1
eunjinii <lucy7xoxo@naver.com>
2023-06-28 10:09:15 +0000

    binary search tree

---
+++ codingtest/udemy/binarysearchtree.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self) -> None:
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                else:
+                    temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                else:
+                    temp = temp.right
+
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if temp.value == value:
+                return True
+            if temp.value < value:
+                temp = temp.right
+            else:
+                temp = temp.left
+        return False
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+print(my_tree.contains(3))
